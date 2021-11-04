7c671f05257cc14fb36ad20cf583289f91c342a3
Fimedur <fimedur.rahman.shuvo@gmail.com>
2021-11-04 18:36:22 +0600

    first commit

---
+++ src/Department.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
+
+export class Department extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>All Department</h1> 
+            </div>
+        );
+    }
+}
+
